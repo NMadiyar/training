199f30f1be279ccd1023bc2fb7f09401f1a29356
Madiyar <madiyarnaizabekov@gmail.com>
2021-05-19 14:07:11 +0600

    added burger menu

--- JS/app.js
+++ JS/app.js
@@ -34,4 +34,12 @@ $(function (){
         }, 700)
     });
 
+//   Nav Toggle =========================
+    let nav = $("#nav");
+    $("#navToggle").on("click", function (event){
+        event.preventDefault();
+        nav.toggleClass("show");
+
+    })
+
 });
